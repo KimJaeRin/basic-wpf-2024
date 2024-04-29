51a92556d97710d7f59006ea651e758fcf189dba
KimJaeRin <158007433+KimJaeRin@users.noreply.github.com>
2024-04-29 17:05:32 +0900

    4월 29일 마무리

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# basic-wpf-2024
-Iot 개발자 WPF 학습 리포지토리
+# WPF 윈폼 개발학습
+IoT 개발자 WPF 학습 리포지토리
+
+## 1일차
+- WPF(Window Presentataion Foundation) 기본학습
+    - Winforms 확장한 WPF 학습
+        - 이전의 Winforms는 이미지 비트맵방식(2D)
+        - WPF 이미지 벡터방식
+        - XAML 화면 디자인 - 안드로이드 개발 시 Java XML 로 화면 디자인과 PtQt로 디자인과 동일
+
+    - XAML(엑스에이엠엘, 재믈)
+        - 여는 태그 <Window>, 닫는 태그 </Window>
+        - 합치면 <Window /> - Window 태그 안에 다른객체가 없단 뜻
+        - 여는 태그와 닫는 태그사이에 다른 태그(객체)를 넣어서 디자인
+
+    - WPF 기본 사용법
+        - Winforms와는 다르게 코딩으로 디자인을 함
+
+    - 레이아웃
+        1. Grid - WPF에서 가장 많이 사용하는 대표적인 레이아웃(중요)
+        2. StackPanel - 스택으로 컨트롤을 쌓는 레이아웃
+        3. Canvas - 미술에서 캔버스와 유사
+        4. DockPanel - 컨트롤을 방향에 따라 도킹시키는 레이아웃
+        5. Margin - 여백기능, 앵커링 같이함(중요)
+
+
+## 2일차
+- WPF 기본학습
+
---
+++ day01/wpf01_basic_app/ex04_wpf_bikeShop/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ex04_wpf_bikeShop
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            // 코드 비하인드에서 Source 속석에 피이지를 넣을때는 UriKind.RelativeOrAbsolute 무조건 추가
+            // 
+            MainFrame.Source = new Uri("/ContactPage.Xaml", UriKind.RelativeOrAbsolute);
+        }
+    }
+}

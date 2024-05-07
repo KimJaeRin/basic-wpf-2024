5f38632ef624052a0b29d04517470cbdc6d7d3dc
KimJaeRin <158007433+KimJaeRin@users.noreply.github.com>
2024-05-07 17:25:09 +0900

    5월 7일

---
+++ day05/wpf05_mvvm_app/ex07_EmployeeMngApp/Helpers/Common.cs
@@ -0,0 +1,20 @@
+﻿using MahApps.Metro.Controls.Dialogs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex07_EmployeeMngApp.Helpers
+{
+    public class Common
+    {
+        public static readonly string CONNSTRING = "Data Source=127.0.0.1;" +
+            "Initial Catalog=EMS;" + "Persist Security Info=True;User ID=ems_user;"
+            +"Password=ems_p@ss;Encrypt=False";
+
+
+        // Metro방식의 다이얼로그 적용을 위해선 반드시 필요
+        public static IDialogCoordinator DialogCoordinator { get; set; }
+    }
+}
---
+++ day05/wpf05_mvvm_app/ex07_EmployeeMngApp/Views/MainView.xaml.cs
@@ -0,0 +1,20 @@
+﻿using ex07_EmployeeMngApp.Helpers;
+using MahApps.Metro.Controls;
+using MahApps.Metro.Controls.Dialogs;
+
+namespace ex07_EmployeeMngApp.Views
+{
+    /// <summary>
+    /// MainViews.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainView : MetroWindow
+    {
+        public MainView()
+        {
+            InitializeComponent();
+
+            Common.DialogCoordinator = DialogCoordinator.Instance;
+            this.DataContext = Common.DialogCoordinator;
+        }
+    }
+}
